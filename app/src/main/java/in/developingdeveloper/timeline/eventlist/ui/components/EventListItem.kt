ca87d95b78e480b5812f2809dc8626936568059c
Abhishek Saxena <5abhisheksaxena@gmail.com>
2023-04-10 02:36:37 +0530

    Add EventListItem.

---
+++ app/src/main/java/in/developingdeveloper/timeline/eventlist/ui/components/EventListItem.kt
@@ -0,0 +1,103 @@
+package `in`.developingdeveloper.timeline.eventlist.ui.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import `in`.developingdeveloper.timeline.core.ui.theme.TimelineTheme
+import `in`.developingdeveloper.timeline.eventlist.ui.models.UIEvent
+import java.time.LocalDateTime
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun EventListItem(event: UIEvent) {
+    Card {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Text(
+                    text = "#${event.id}",
+                    color = if (isSystemInDarkTheme()) {
+                        MaterialTheme.colorScheme.onSurface
+                    } else {
+                        Color(0xFF808080)
+                    },
+                    style = MaterialTheme.typography.bodySmall,
+                )
+            }
+
+            Text(
+                text = event.title,
+                style = MaterialTheme.typography.titleMedium,
+            )
+
+            FlowRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+            ) {
+                event.tags.forEach { tag ->
+                    Text(
+                        text = tag,
+                        style = MaterialTheme.typography.bodySmall,
+                        modifier = Modifier.background(
+                            color =
+                            if (isSystemInDarkTheme()) {
+                                MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f)
+                            } else {
+                                Color(0xFFEDE9E8)
+                            },
+                        ),
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(
+    name = "Night Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Preview(
+    name = "Day Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+)
+@Composable
+@Suppress("UnusedPrivateMember", "MagicNumber")
+private fun EventListItemPreview() {
+    val event = UIEvent(
+        "123",
+        "Sample title",
+        listOf("#Android", "#Kotlin"),
+        LocalDateTime.now(),
+        LocalDateTime.now(),
+    )
+
+    TimelineTheme {
+        Surface {
+            EventListItem(event)
+        }
+    }
+}
