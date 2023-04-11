be1ca34ac54ce3fc75da4740a23bae8ce7df3c23
Abhishek Saxena <5abhisheksaxena@gmail.com>
2023-04-12 05:10:12 +0530

    Updating EventList screen with the data received from the use case.

--- app/src/main/java/in/developingdeveloper/timeline/eventlist/ui/EventListScreen.kt
+++ app/src/main/java/in/developingdeveloper/timeline/eventlist/ui/EventListScreen.kt
@@ -1,8 +1,11 @@
 package `in`.developingdeveloper.timeline.eventlist.ui
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ramcosta.composedestinations.annotation.Destination
@@ -16,6 +19,14 @@ fun EventListScreen(
     viewModel: EventListViewModel = hiltViewModel(),
 ) {
     val viewState by viewModel.viewState.collectAsStateWithLifecycle()
+
+    val context = LocalContext.current
+
+    LaunchedEffect(key1 = viewState.errorMessage) {
+        viewState.errorMessage?.let {
+            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+        }
+    }
 
     EventListContent(
         viewState = viewState,
--- app/src/main/java/in/developingdeveloper/timeline/eventlist/ui/EventListViewModel.kt
+++ app/src/main/java/in/developingdeveloper/timeline/eventlist/ui/EventListViewModel.kt
@@ -1,15 +1,71 @@
 package `in`.developingdeveloper.timeline.eventlist.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import `in`.developingdeveloper.timeline.eventlist.domain.models.Event
+import `in`.developingdeveloper.timeline.eventlist.domain.usescases.GetAllEventsUseCase
 import `in`.developingdeveloper.timeline.eventlist.ui.models.EventListViewState
+import `in`.developingdeveloper.timeline.eventlist.ui.models.UIEvent
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-class EventListViewModel @Inject constructor() : ViewModel() {
+class EventListViewModel @Inject constructor(
+    private val getAllEventsUseCase: GetAllEventsUseCase,
+) : ViewModel() {
 
     private val _viewState = MutableStateFlow(EventListViewState.Initial)
     val viewState = _viewState.asStateFlow()
+
+    init {
+        getAllEvents()
+    }
+
+    private fun getAllEvents() {
+        getAllEventsUseCase.invoke()
+            .flowOn(Dispatchers.IO)
+            .distinctUntilChanged()
+            .onStart {
+                _viewState.update { it.copy(loading = true) }
+            }
+            .onEach { result ->
+                _viewState.update { getViewStateForEventListResult(result) }
+            }
+            .launchIn(viewModelScope)
+    }
+
+    private fun getViewStateForEventListResult(result: Result<List<Event>>): EventListViewState {
+        val currentViewState = _viewState.value
+        return result.fold(
+            onSuccess = { events ->
+                val uiEvents = events.toUiEvents()
+                currentViewState.copy(events = uiEvents, loading = false)
+            },
+            onFailure = {
+                val message = it.message ?: "Something went wrong."
+                currentViewState.copy(loading = false, errorMessage = message)
+            },
+        )
+    }
+}
+
+private fun List<Event>.toUiEvents(): List<UIEvent> = this.map(Event::toUiEvent)
+
+private fun Event.toUiEvent(): UIEvent {
+    return UIEvent(
+        id = this.id,
+        title = this.title,
+        tags = this.tags,
+        date = this.date,
+        createdOn = this.createdOn,
+    )
 }
