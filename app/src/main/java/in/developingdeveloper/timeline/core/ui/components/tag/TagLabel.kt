1fca8885c74ea3c4a515a5fba92d2af13d24ddb0
Abhishek Saxena <5abhisheksaxena@gmail.com>
2023-04-16 04:31:14 +0530

    Fix tag background color for dark theme.

--- app/src/main/java/in/developingdeveloper/timeline/core/ui/components/tag/TagLabel.kt
+++ app/src/main/java/in/developingdeveloper/timeline/core/ui/components/tag/TagLabel.kt
@@ -1,8 +1,10 @@
 package `in`.developingdeveloper.timeline.core.ui.components.tag
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,7 +20,14 @@ fun TagLabel(
         text = label,
         modifier = modifier
             .padding(4.dp)
-            .background(TagBackground, RoundedCornerShape(28.dp))
+            .background(
+                color = if (isSystemInDarkTheme()) {
+                    MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f)
+                } else {
+                    TagBackground
+                },
+                shape = RoundedCornerShape(28.dp),
+            )
             .padding(horizontal = 20.dp, vertical = 2.dp),
     )
 }
