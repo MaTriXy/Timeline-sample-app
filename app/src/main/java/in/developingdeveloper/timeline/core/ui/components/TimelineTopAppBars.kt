0a842ff2b55219cec2397bacc185b323d63435f6
Abhishek Saxena <5abhisheksaxena@gmail.com>
2023-04-16 02:38:42 +0530

    Adding StartAlignedTopAppBar.

--- app/src/main/java/in/developingdeveloper/timeline/core/ui/components/TimelineTopAppBars.kt
+++ app/src/main/java/in/developingdeveloper/timeline/core/ui/components/TimelineTopAppBars.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
@@ -21,6 +22,29 @@ fun TimelineCenterAlignedTopAppBar(
     scrollBehavior: TopAppBarScrollBehavior? = null,
 ) {
     CenterAlignedTopAppBar(
+        title = {
+            Text(text = title)
+        },
+        modifier = modifier,
+        navigationIcon = navigationIcon,
+        actions = actions,
+        windowInsets = windowInsets,
+        colors = colors,
+        scrollBehavior = scrollBehavior,
+    )
+}
+
+@Composable
+fun TimelineStartAlignedTopAppBar(
+    title: String,
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable () -> Unit = {},
+    actions: @Composable RowScope.() -> Unit = {},
+    windowInsets: WindowInsets = TopAppBarDefaults.windowInsets,
+    colors: TopAppBarColors = TopAppBarDefaults.centerAlignedTopAppBarColors(),
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+) {
+    TopAppBar(
         title = {
             Text(text = title)
         },
