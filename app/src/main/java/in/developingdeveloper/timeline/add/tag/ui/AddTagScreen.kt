4e927798e80cb338c2daefc51a91557e79761033
Abhishek Saxena <5abhisheksaxena@gmail.com>
2023-04-21 00:36:33 +0530

    Displaying snackbar when tag is added successfully.

--- app/src/main/java/in/developingdeveloper/timeline/add/tag/ui/AddTagScreen.kt
+++ app/src/main/java/in/developingdeveloper/timeline/add/tag/ui/AddTagScreen.kt
@@ -1,22 +1,32 @@
 package `in`.developingdeveloper.timeline.add.tag.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.result.ResultBackNavigator
 
 @Composable
 @Destination
 fun AddTagScreen(
     navigator: DestinationsNavigator,
+    resultNavigator: ResultBackNavigator<String>,
     viewModel: AddTagViewModel = hiltViewModel(),
 ) {
     val viewState by viewModel.viewState.collectAsStateWithLifecycle()
 
     val keyboardController = LocalSoftwareKeyboardController.current
+
+    LaunchedEffect(viewState.isCompleted) {
+        resultNavigator.navigateBack(
+            result = "Tag added successfully!",
+            onlyIfResumed = true,
+        )
+    }
 
     AddTagContent(
         viewState = viewState,
