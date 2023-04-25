e4c1efd94b0e8edc54b8c89188bf1fa15fd099e9
Abhishek Saxena <5abhisheksaxena@gmail.com>
2023-04-26 02:13:22 +0530

    Displaying progress bar in loading state.

--- app/src/main/java/in/developingdeveloper/timeline/add/event/ui/AddEventContent.kt
+++ app/src/main/java/in/developingdeveloper/timeline/add/event/ui/AddEventContent.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -44,6 +46,14 @@ fun AddEventContent(
                 .fillMaxSize()
                 .padding(16.dp),
         ) {
+            if (viewState.isLoading) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(),
+                )
+            }
+
             AddEventForm(
                 isFormEnabled = viewState.formEnabled,
                 form = viewState.form,
