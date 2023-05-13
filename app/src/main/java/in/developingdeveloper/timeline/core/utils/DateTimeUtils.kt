ec9daddce9347c6a73e68cddefd3c59c603d75ee
Abhishek Saxena <5abhisheksaxena@gmail.com>
2023-05-14 01:36:52 +0530

    Moving date formatter util function to DateTimeUtils.

--- app/src/main/java/in/developingdeveloper/timeline/core/utils/DateTimeUtils.kt
+++ app/src/main/java/in/developingdeveloper/timeline/core/utils/DateTimeUtils.kt
@@ -5,9 +5,12 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 private const val UTC = "UTC"
 private val zoneId = ZoneId.of(UTC)
+private const val UI_DATE_FORMAT = "dd MMMM yyyy"
+private val uiDateTimeFormatter: DateTimeFormatter? = DateTimeFormatter.ofPattern(UI_DATE_FORMAT)
 
 fun Long.toLocalDateTime(): LocalDateTime {
     return Instant
@@ -26,4 +29,8 @@ fun LocalDateTime.toEpochMilli(): Long {
 
 fun LocalDate.toLocalDateTime(): LocalDateTime {
     return LocalDateTime.of(this, LocalTime.MIDNIGHT)
+}
+
+fun LocalDateTime.formatDateForUI(): String {
+    return this.format(uiDateTimeFormatter) ?: this.toString()
 }
