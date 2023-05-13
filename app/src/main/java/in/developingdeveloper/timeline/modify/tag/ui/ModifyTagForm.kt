10aba4582a61bb4a2d49b4e8091a7ebfeae79a9c
Abhishek Saxena <5abhisheksaxena@gmail.com>
2023-05-14 02:22:43 +0530

    Setting singleLine and capitalization type to Words.

--- app/src/main/java/in/developingdeveloper/timeline/modify/tag/ui/ModifyTagForm.kt
+++ app/src/main/java/in/developingdeveloper/timeline/modify/tag/ui/ModifyTagForm.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
@@ -19,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import `in`.developingdeveloper.timeline.R
@@ -92,6 +94,10 @@ private fun LabelInputField(
         onTextChange = onLabelValueChange,
         errorMessage = errorMessage,
         modifier = modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions.Default.copy(
+            capitalization = KeyboardCapitalization.Words,
+        ),
+        singleLine = true,
     )
 }
 
